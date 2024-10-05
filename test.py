97b3230a5bfafed37273a94854cf2ec34215d709
wise <fomin27ivan@gmail.com>
2024-10-05 13:16:43 +0500

    delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-data = {'question_1': 'Г', 'question_2': 'Б', 'question_3': 'В', 'question_4': 'Г', 	'question_5': 'А', 'question_6': 'Б', 'question_7': 'В', 'question_8': 'Г', 'question_9': 'Г', 'question_10': 'Г'}
-
-
-     
