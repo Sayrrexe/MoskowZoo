f24a4c3040dfcf0c1881cbe00b8cf3d196f245ec
wise <fomin27ivan@gmail.com>
2024-10-25 20:52:20 +0500

    Changes

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardMarkup, InlineKeyboardButton)
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
-
+from config import URL
 
 menu = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Пройти Викторину'),],
@@ -13,13 +13,31 @@ menu = ReplyKeyboardMarkup(keyboard=[
 
 start_quiz = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Начать Викторину!')]
-])
+],
+                           resize_keyboard=True,
+                           input_field_placeholder='Выберите пункт меню.')
 
 start_quiz_button = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Начать!')]
-])
+],
+                           resize_keyboard=True,
+                           input_field_placeholder='Выберите пункт меню.')
 
 answers_keyboard = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='А'), KeyboardButton(text='Б')],
     [KeyboardButton(text='В'), KeyboardButton(text='Г')]
-])
+],
+                           resize_keyboard=True,
+                           input_field_placeholder='Выберите пункт меню.')
+
+async def share_kb(key):
+    keyboard = InlineKeyboardBuilder()
+    keyboard.add(InlineKeyboardButton(text='Поделится!', callback_data=f'share_{key}'))
+    keyboard.add(InlineKeyboardButton(text='начать с начала', callback_data='menu'))
+    return keyboard.adjust(1).as_markup()
+
+link_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Пройти тест!", url=URL,)]
+    ]
+)
--- app/user.py
+++ app/user.py
@@ -1,32 +1,37 @@
 from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery, InputFile
+from aiogram.types import Message, CallbackQuery, InputFile, FSInputFile
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
 
+from app.keyboards import link_kb, start_quiz
 from app.quiz_module import answer_render
 from app.states import Victory_State
 import app.keyboards as kb
-from config import question1_text, question2_text
+from config import (question1_text, question2_text, question3_text, question4_text, question5_text,
+                    question6_text, question7_text, question8_text, question9_text,
+                    question10_text, answer_a, answer_d, answer_c, answer_b, share_a, share_c,
+                    share_d, share_b, welcome_text, description_text, info_text, benifits_text,
+                    quiz_start_text)
 
 user = Router()
 
 
 @user.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Добро пожаловать в неофициальный бот "Московского Зоопарка"')
-    await message.answer('Бот Призван Помочь вам с выбором животного для опеки по программе "Опекунство в Московском зоопарке"', reply_markup=kb.menu)
+    await message.answer(welcome_text)
+    await message.answer(description_text, reply_markup=kb.menu)
 
-@user.message(F.text =='Что такое Опекунство?')
+@user.message(F.text.lower() =='что такое опекунство?')
 async def text_prems(message: Message):
-    await message.answer('Возьмите животное под опеку! Участие в программе «Клуб друзей зоопарка» — это помощь в содержании наших обитателей, а также ваш личный вклад в дело сохранения биоразнообразия Земли и развитие нашего зоопарка.\n\nОсновная задача Московского зоопарка с самого начала его существования это — сохранение биоразнообразия планеты. Когда вы берете под опеку животное, вы помогаете нам в этом благородном деле.\n\nПри нынешних темпах развития цивилизации к 2050 году с лица Земли могут исчезнуть около 10 000 биологических видов. Московский зоопарк вместе с другими зоопарками мира делает все возможное, чтобы сохранить их. Традиция опекать животных в Московском зоопарке возникло с момента его создания в 1864г. Такая практика есть и в других зоопарках по всему миру.\n\nВ настоящее время опекуны объединились в неформальное сообщество — Клуб друзей Московского зоопарка. Программа «Клуб друзей» дает возможность опекунам ощутить свою причастность к делу сохранения природы, участвовать в жизни Московского зоопарка и его обитателей, видеть конкретные результаты своей деятельности. Опекать – значит помогать любимым животным. Можно взять под крыло любого обитателя Московского зоопарка, в том числе и того, кто живет за городом – в Центре воспроизводства редких видов животных под Волоколамском. Там живут и размножаются виды, которых нет в городской части зоопарка: величественные журавли стерхи, забавные дрофы, исчезнувшая в природе лошадь Пржевальского, изящные антилопы бонго и многие другие. Можете съездить на экскурсию в Центр и познакомиться с обитателями.', reply_markup=kb.menu)
+    await message.answer(info_text, reply_markup=kb.menu)
 
 @user.message(F.text =='Преимущества статуса опекуна')
 async def text_prems(message: Message):
-    await message.answer('Преимущества статуса опекуна:\n·Сертификат опеки, подтверждающий Ваш статус опекуна животного\n·Табличка на вольере с именем опекуна данного животного\n·Карта друга Московского зоопарка, дающая множество преимуществ', reply_markup=kb.menu) 
+    await message.answer(benifits_text, reply_markup=kb.menu)
     
-@user.message(F.text =='Пройти Викторину')
+@user.message(F.text.lower() =='пройти викторину')
 async def start_user(message: Message, state: FSMContext):
-        await message.answer('Так быстро? Ладно!\n Вам нужно будет отвечать на вопросы при помощи встроенной клавиатуры ( вы можете найти её справа в строке ввода)\nПерейдите туда и начинайте', reply_markup=kb.start_quiz_button)
+        await message.answer(quiz_start_text, reply_markup=kb.start_quiz_button)
         await state.set_state(Victory_State.start)
         
 @user.message(Victory_State.start)
@@ -43,49 +48,49 @@ async def question1(message: Message, state: FSMContext):
 @user.message(Victory_State.question_2)
 async def question1(message: Message, state: FSMContext):
     await state.update_data(question_2 = message.text)
-    await message.answer('3 из 10\nКак вы относитесь к общению с другими людьми?\nA) Предпочитаю глубокие и спокойные беседы.\nB) Люблю общаться и заводить новые знакомства.\nC) Ценю дружеские связи и активное взаимодействие.\nD) Общаюсь, когда это необходимо, но ценю личное пространство.', reply_markup=kb.answers_keyboard)
+    await message.answer(question3_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_3)
     
 @user.message(Victory_State.question_3)
 async def question1(message: Message, state: FSMContext):
     await state.update_data(question_3 = message.text)
-    await message.answer('4 из 10\nКак вы воспринимаете трудности?\nA) Стараюсь найти мирное решение.\nB) Применяю решительные меры для преодоления.\nC) Использую свою ловкость и смекалку.\nD) Подхожу к проблемам с осторожностью и расчетом.', reply_markup=kb.answers_keyboard)
+    await message.answer(question4_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_4)
     
 @user.message(Victory_State.question_4)
 async def question1(message: Message, state: FSMContext):
     await state.update_data(question_4 = message.text)
-    await message.answer("5 из 10\nКак вы относитесь к свободе и независимости?\nA) Ценю стабильность и комфорт.\nB) Свобода — это возможность для приключений.\nC) Независимость важна для самовыражения.\nD) Свобода — это возможность действовать по своему усмотрению.", reply_markup=kb.answers_keyboard)
+    await message.answer(question5_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_5)
     
 @user.message(Victory_State.question_5)
 async def question1(message: Message, state: FSMContext):
     await state.update_data(question_5 = message.text)
-    await message.answer('6 из 10\nКак вы видите свою роль в группе или команде?\nA) Спокойный наблюдатель, который поддерживает других.\nB) Лидер, который вдохновляет и ведет за собой.\nC) Активный участник, который приносит идеи.\nD) Стратег, который помогает находить решения.', reply_markup=kb.answers_keyboard)
+    await message.answer(question6_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_6)
     
 @user.message(Victory_State.question_6)
 async def question1(message: Message, state: FSMContext):
     await state.update_data(question_6 = message.text)
-    await message.answer('7 из 10\nКак вы относитесь к риску?\nA) Предпочитаю избегать рисков и действовать осторожно.\nB) Готов(а) принимать риски ради новых возможностей.\nC) Оцениваю риски и принимаю взвешенные решения.\nD) Считаю, что риск — это часть жизни.', reply_markup=kb.answers_keyboard)
+    await message.answer(question7_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_7)
     
 @user.message(Victory_State.question_7)
 async def question1(message: Message, state: FSMContext):
     await state.update_data(question_7 = message.text)
-    await message.answer('8 из 10\nКак вы воспринимаете успех?\nA) Успех — это гармония и спокойствие.\nB) Успех — это достижения и новые горизонты.\nC) Успех — это признание и уважение.\nD) Успех — это постоянное развитие и самосовершенствование.', reply_markup=kb.answers_keyboard)
+    await message.answer(question8_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_8)
     
 @user.message(Victory_State.question_8)
 async def question1(message: Message, state: FSMContext):
     await state.update_data(question_8 = message.text)
-    await message.answer('9 из 10\nКак вы относитесь к обучению и саморазвитию?\nA) Считаю это важной частью жизни.\nB) Люблю изучать новые темы и навыки.\nC) Участвую в курсах и семинарах.\nD) Предпочитаю учиться на практике.', reply_markup=kb.answers_keyboard)
+    await message.answer(question9_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_9)
     
 @user.message(Victory_State.question_9)
 async def question1(message: Message, state: FSMContext):
     await state.update_data(question_9 = message.text)
-    await message.answer('Крайний вопрос!\nКак вы видите свою связь с природой?\nA) Я чувствую себя частью природы и ценю её.\nB) Я стремлюсь исследовать и открывать новое.\nC) Я нахожу вдохновение в её красоте.\nD) Я уважаю природу и стараюсь жить в гармонии с ней.', reply_markup=kb.answers_keyboard)
+    await message.answer(question10_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_10)
     
 @user.message(Victory_State.question_10)
@@ -93,6 +98,41 @@ async def question1(message: Message, state: FSMContext):
     await state.update_data(question_10 = message.text)
     await message.answer('Анализ Данных...')
     data = await state.get_data()
-    Message = await answer_render(data)
-    await message.delete()
-    await message.answer(Message)
+    key = await answer_render(data)
+    if key == 'a':
+        image, text = answer_a[0], answer_a[1]
+        await message.answer_photo(photo=FSInputFile(image, filename="pingvin"),
+                                   caption=text, reply_markup=await kb.share_kb(key))
+    if key == 'b':
+        image, text = answer_b[0], answer_b[1]
+        await message.answer_photo(photo=FSInputFile(image, filename="puma"),
+                                   caption=text, reply_markup=await kb.share_kb(key))
+    if key == 'c':
+        image, text = answer_c[0], answer_c[1]
+        await message.answer_photo(photo=FSInputFile(image, filename="bars"),
+                                   caption=text, reply_markup=await kb.share_kb(key))
+    if key == 'd':
+        image, text = answer_d[0], answer_d[1]
+        await message.answer_photo(photo=FSInputFile(image, filename="medoed"),
+                                   caption=text, reply_markup= await kb.share_kb(key))
+
+@user.callback_query(F.data.startswith("share_"))
+async def share(callback: CallbackQuery):
+    key = callback.data.split("_")[1]
+    await callback.message.delete()
+    if key == 'a':
+        image, text = share_a[0], share_a[1]
+        await callback.message.answer_photo(photo=FSInputFile(image, filename="Car"),
+                                   caption=text, reply_markup=link_kb)
+    if key == 'b':
+        image, text = share_b[0], share_b[1]
+        await callback.message.answer_photo(photo=FSInputFile(image, filename="Car"),
+                                   caption=text, reply_markup=link_kb)
+    if key == 'c':
+        image, text = share_c[0], share_c[1]
+        await callback.message.answer_photo(photo=FSInputFile(image, filename="Car"),
+                                   caption=text, reply_markup=link_kb)
+    if key == 'd':
+        image, text = share_d[0], share_d[1]
+        await callback.message.answer_photo(photo=FSInputFile(image, filename="Car"),
+                                   caption=text, reply_markup=link_kb)
