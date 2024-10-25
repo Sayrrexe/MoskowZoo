3e89738da3c19efd3ecdee9a0ef37ddced5e7495
wise <fomin27ivan@gmail.com>
2024-10-25 21:03:13 +0500

    Changes

--- app/user.py
+++ app/user.py
@@ -10,7 +10,7 @@ import app.keyboards as kb
 from config import (question1_text, question2_text, question3_text, question4_text, question5_text,
                     question6_text, question7_text, question8_text, question9_text,
                     question10_text, answer_a, answer_d, answer_c, answer_b, share_a, share_c,
-                    share_d, share_b, welcome_text, description_text, info_text, benifits_text,
+                    share_d, share_b, welcome_text, description_text, info_text, benefits_text,
                     quiz_start_text)
 
 user = Router()
@@ -27,13 +27,23 @@ async def text_prems(message: Message):
 
 @user.message(F.text =='Преимущества статуса опекуна')
 async def text_prems(message: Message):
-    await message.answer(benifits_text, reply_markup=kb.menu)
+    await message.answer(benefits_text, reply_markup=kb.menu)
     
 @user.message(F.text.lower() =='пройти викторину')
 async def start_user(message: Message, state: FSMContext):
         await message.answer(quiz_start_text, reply_markup=kb.start_quiz_button)
         await state.set_state(Victory_State.start)
-        
+
+async def check_answer(message):
+    text = str(message.text)
+    if text == 'А' or text ==  'Б' or text == 'В' or text == 'Г':
+        return True
+    else:
+        await message.answer('Используйте встроенную клавиатуру!')
+        return False
+
+
+
 @user.message(Victory_State.start)
 async def question1(message: Message, state: FSMContext):
     await message.answer(question1_text, reply_markup=kb.answers_keyboard)
@@ -41,60 +51,80 @@ async def question1(message: Message, state: FSMContext):
 
 @user.message(Victory_State.question_1)
 async def question1(message: Message, state: FSMContext):
+    if not await check_answer(message):
+        return
     await state.update_data(question_1 = message.text)
     await message.answer(question2_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_2)
 
 @user.message(Victory_State.question_2)
 async def question1(message: Message, state: FSMContext):
+    if not await check_answer(message):
+        return
     await state.update_data(question_2 = message.text)
     await message.answer(question3_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_3)
     
 @user.message(Victory_State.question_3)
 async def question1(message: Message, state: FSMContext):
+    if not await check_answer(message):
+        return
     await state.update_data(question_3 = message.text)
     await message.answer(question4_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_4)
     
 @user.message(Victory_State.question_4)
 async def question1(message: Message, state: FSMContext):
+    if not await check_answer(message):
+        return
     await state.update_data(question_4 = message.text)
     await message.answer(question5_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_5)
     
 @user.message(Victory_State.question_5)
 async def question1(message: Message, state: FSMContext):
+    if not await check_answer(message):
+        return
     await state.update_data(question_5 = message.text)
     await message.answer(question6_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_6)
     
 @user.message(Victory_State.question_6)
 async def question1(message: Message, state: FSMContext):
+    if not await check_answer(message):
+        return
     await state.update_data(question_6 = message.text)
     await message.answer(question7_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_7)
     
 @user.message(Victory_State.question_7)
 async def question1(message: Message, state: FSMContext):
+    if not await check_answer(message):
+        return
     await state.update_data(question_7 = message.text)
     await message.answer(question8_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_8)
     
 @user.message(Victory_State.question_8)
 async def question1(message: Message, state: FSMContext):
+    if not await check_answer(message):
+        return
     await state.update_data(question_8 = message.text)
     await message.answer(question9_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_9)
     
 @user.message(Victory_State.question_9)
 async def question1(message: Message, state: FSMContext):
+    if not await check_answer(message):
+        return
     await state.update_data(question_9 = message.text)
     await message.answer(question10_text, reply_markup=kb.answers_keyboard)
     await state.set_state(Victory_State.question_10)
     
 @user.message(Victory_State.question_10)
 async def question1(message: Message, state: FSMContext):
+    if not await check_answer(message):
+        return
     await state.update_data(question_10 = message.text)
     await message.answer('Анализ Данных...')
     data = await state.get_data()
